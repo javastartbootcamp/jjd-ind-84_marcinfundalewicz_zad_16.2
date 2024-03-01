800b0c0220e087d5ceb2559ae5c146b4b8bc02f3
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-03-01 15:11:04 +0100

    Poprawki 1

--- src/main/java/pl/javastart/task/Format.java
+++ src/main/java/pl/javastart/task/Format.java
@@ -1,17 +1,23 @@
 package pl.javastart.task;
 
 public enum Format {
-    FORMAT_1("yyyy-MM-dd HH:mm:ss"),
-    FORMAT_2("yyyy-MM-dd"),
-    FORMAT_3("dd.MM.yyyy HH:mm:ss");
+    FORMAT_1("yyyy-MM-dd HH:mm:ss", true),
+    FORMAT_2("yyyy-MM-dd", false),
+    FORMAT_3("dd.MM.yyyy HH:mm:ss", true);
 
     private String pattern;
+    private boolean containsTime;
 
-    Format(String pattern) {
+    Format(String pattern, boolean containsTime) {
         this.pattern = pattern;
+        this.containsTime = containsTime;
     }
 
     public String getPattern() {
         return pattern;
+    }
+
+    public boolean isContainsTime() {
+        return containsTime;
     }
 }
--- src/main/java/pl/javastart/task/TimeManager.java
+++ src/main/java/pl/javastart/task/TimeManager.java
@@ -1,10 +1,12 @@
 package pl.javastart.task;
 
-import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -38,24 +40,44 @@ public class TimeManager {
         DateTimeFormatter dateTimeFormatter;
         Format[] formatsValues = Format.values();
         for (Format formatsValue : formatsValues) {
-            dateTimeFormatter = DateTimeFormatter.ofPattern(formatsValue.getPattern());
+            if (!formatsValue.isContainsTime()) {
+                dateTimeFormatter = new DateTimeFormatterBuilder()
+                        .appendPattern("yyyy-MM-dd")
+                        .optionalStart()
+                        .appendPattern(" HH:mm")
+                        .optionalEnd()
+                        .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+                        .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+                        .toFormatter();
+
+            } else {
+                dateTimeFormatter = DateTimeFormatter.ofPattern(formatsValue.getPattern());
+            }
+
             formatters.add(dateTimeFormatter);
+
         }
+
         return formatters;
     }
 
-    public DateTimeFormatter getDateTimeFormatter(String date) throws DateTimeException {
+    public DateTimeFormatter getDateTimeFormatter(String dateString) throws DateTimeParseException {
         DateTimeFormatter formatter = null;
         LocalDateTime localDateTime = null;
         List<DateTimeFormatter> dateTimeFormatters = initFormattersList();
         for (DateTimeFormatter dateTimeFormatter : dateTimeFormatters) {
-            localDateTime = LocalDateTime.parse(date, dateTimeFormatter);
-            formatter = dateTimeFormatter;
-            break;
+            try {
+                localDateTime = LocalDateTime.parse(dateString, dateTimeFormatter);
+                formatter = dateTimeFormatter;
+                break;
+            } catch (DateTimeParseException e) {
+                //
+            }
         }
         if (localDateTime == null) {
-            throw new DateTimeException("Nieprawidłowy format");
+            throw new DateTimeParseException("Nieprawidłowy format daty", dateString, 0);
         }
+
         return formatter;
     }
 }
